85a7434dfb2ed627e8c74d950f4c3ca43355fcaa
karan arora <nsit_karan@yahoo.com>
2023-05-26 20:55:01 +0530

    updated logic for conversations

--- src/basic_langchain.py
+++ src/basic_langchain.py
@@ -3,6 +3,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
+from langchain import ConversationChain
 
 # temperature - randomness in the output
 llm = OpenAI(temperature=0.9)
@@ -26,8 +27,15 @@ def basic_agent():
     agent = initialize_agent(tools, llm, "zero-shot-react-description", verbose=True)
     agent.run("Who won the ipl match played on may24 2023")
 
+def conversation_agent():
+    conversation = ConversationChain(llm=llm, verbose=True)
+    conversation.predict(input="Hi lets start")
+    conversation.predict(input="how do i start with learning machine learning")
+    conversation.predict(input="can you suggest relevant free lectures that i can follow")
+    conversation.predict(input="thanks. thats all i have for today")
 
 
 basic_query()
 basic_prompt()
 basic_agent()
+conversation_agent()
