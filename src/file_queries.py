f0fc59040100108f23dd0fcf0e3dc5735b5a8118
karan arora <nsit_karan@yahoo.com>
2023-05-28 20:16:52 +0530

    query run based on 2 docs as input

--- src/file_queries.py
+++ src/file_queries.py
@@ -3,6 +3,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
+from langchain.chains import RetrievalQA,RetrievalQAWithSourcesChain
 from langchain.document_loaders import DirectoryLoader
 
 import magic
@@ -23,3 +24,15 @@ texts = text_splitter.split_documents(documents)
 
 # Create embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+
+# Local in memory db
+docsearch = Chroma.from_documents(texts, embeddings)
+
+#qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch, top_k_docs_for_context=1)
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever(search_kwargs={"k": 1}))
+
+# This one works:
+#query = "can you tell the summary of these docs?"
+
+query = "how can someone generate new ideas?"
+print(qa.run(query))
